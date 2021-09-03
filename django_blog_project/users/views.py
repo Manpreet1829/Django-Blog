cd0b635c31d4e4be8d7a05d4ebc723197ce5ec8d
Manpreet Kaur <manpi.1829@gmail.com>
2021-09-03 13:10:48 -0600

    authentication system

--- django_blog_project/users/views.py
+++ django_blog_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
@@ -9,10 +10,14 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"account created for {username}")
-            return redirect('blog-home')
+            # username = form.cleaned_data.get('username')
+            messages.success(request, f"your account has been created! Please login")
+            return redirect('login')
     else:
         form = UserRegistrationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
