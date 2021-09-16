03e832bd57d00419fca2236c238869038fae0a3b
Manpreet Kaur <manpi.1829@gmail.com>
2021-09-16 16:08:52 -0600

    post update view

--- django_blog_project/blog/urls.py
+++ django_blog_project/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
     path('about/', views.about, name = 'blog-about'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name="post-create")
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
 
 #<app>/<model>_<view>.html  is the default name which postListView.as_view() will look for.
