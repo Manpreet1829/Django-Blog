47c06f0a4fb06f01b40cccb7c35802614e74f84b
Manpreet Kaur <manpi.1829@gmail.com>
2021-09-16 15:42:54 -0600

    default_class_views

--- django_blog_project/blog/urls.py
+++ django_blog_project/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
+    path('', PostListView.as_view(), name = 'blog-home'),
     path('about/', views.about, name = 'blog-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
+
+#<app>/<model>_<view>.html  is the default name which postListView.as_view() will look for.
--- django_blog_project/blog/views.py
+++ django_blog_project/blog/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
@@ -10,5 +11,21 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+    model = Post
+    # <app>/<model>_<view>.html  is the default name which postListView.as_view() will look for. to change that, set
+    # template_name
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):   # this will look for blog/post_detail.html template by default
+    model = Post
+
+
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
+
+
+
